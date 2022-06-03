cdc8b6350f64a095a8e35cad2432e5a340b56b52
Ervin Vladic <erivn.vladic@stu.ibu.edu.ba>
2022-06-03 17:05:31 +0100

    adding register layout

---
+++ app/src/main/java/com/example/sportify/LoginActivity.java
@@ -0,0 +1,4 @@
+package com.example.sportify;
+
+public class LoginActivity {
+}
--- app/src/main/java/com/example/sportify/MainActivity.java
+++ app/src/main/java/com/example/sportify/MainActivity.java
@@ -16,13 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
 
     }
 
-}
